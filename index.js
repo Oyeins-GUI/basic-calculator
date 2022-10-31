4136cf4d158801bebbc96eb463cb4c183408d1b5
Oyeins-GUI <oyeinx7@gmail.com>
2022-10-31 12:49:39 +0000

    save feature added

--- index.js
+++ index.js
@@ -1,112 +1,125 @@
 // fetched elements
-const addition = document.querySelector('.add')
-const subtraction = document.querySelector('.sub')
-const multiplication = document.querySelector('.mul')
-const division = document.querySelector('.div')
-const clearBtn = document.querySelector('.clear-btn')
-let firstInput = document.querySelector('#first-input')
-let secondInput = document.querySelector('#second-input')
-let result = document.querySelector('.math-result')
-let errMsg1 = document.querySelector('.first-input-err')
-let errMsg2 = document.querySelector('.second-input-err')
-let saveBtn = document.querySelector('.save-btn')
-let savedCal = document.querySelector('.saved-cal')
+const addition = document.querySelector(".add");
+const subtraction = document.querySelector(".sub");
+const multiplication = document.querySelector(".mul");
+const division = document.querySelector(".div");
+const clearBtn = document.querySelector(".clear-btn");
+let firstInput = document.querySelector("#first-input");
+let secondInput = document.querySelector("#second-input");
+let result = document.querySelector(".math-result");
+let errMsg1 = document.querySelector(".first-input-err");
+let errMsg2 = document.querySelector(".second-input-err");
+let saveBtn = document.querySelector(".save-btn");
+let savedCal = document.querySelector(".saved-cal");
+let clickedOperation;
 
 // click event listeners on fetched elements
-saveBtn.addEventListener('click', () => {
-    // let firstInput = document.querySelector('#first-input').value
-    // let secondInput = document.querySelector('#second-input').value
-    // let resultText = result.textContent
+addition.addEventListener("click", () => {
+    let firstInput = parseFloat(document.querySelector("#first-input").value);
+    let secondInput = parseFloat(document.querySelector("#second-input").value);
+    let result = document.querySelector(".math-result");
 
-    // if (firstInput == '' && secondInput == '') {
-    //     savedCal.textContent += ''
-    // } else if (firstInput == '' || secondInput == '') {
-    //     savedCal.textContent += ''
-    // } else {
-    //     savedCal.textContent += "Still to come"
-    // }
-    alert("This feature is yet to come.")
-})
+    const sum = firstInput + secondInput;
+    result.textContent = sum;
+    clickedOperation = addition.textContent;
 
-addition.addEventListener('click', () => {
-    let firstInput = parseFloat(document.querySelector('#first-input').value)
-    let secondInput = parseFloat(document.querySelector('#second-input').value)
-    let result = document.querySelector('.math-result')
+    checkInput();
+});
 
-    const sum = firstInput + secondInput
-    result.textContent = sum
+subtraction.addEventListener("click", () => {
+    let firstInput = parseFloat(document.querySelector("#first-input").value);
+    let secondInput = parseFloat(document.querySelector("#second-input").value);
+    let result = document.querySelector(".math-result");
 
-    checkInput()
-})
+    const difference = firstInput - secondInput;
+    result.textContent = difference;
+    clickedOperation = subtraction.textContent;
 
-subtraction.addEventListener('click', () => {
-    let firstInput = parseFloat(document.querySelector('#first-input').value)
-    let secondInput = parseFloat(document.querySelector('#second-input').value)
-    let result = document.querySelector('.math-result')
+    checkInput();
+});
 
-    const sum = firstInput - secondInput
-    result.textContent = sum
+multiplication.addEventListener("click", () => {
+    let firstInput = parseFloat(document.querySelector("#first-input").value);
+    let secondInput = parseFloat(document.querySelector("#second-input").value);
+    let result = document.querySelector(".math-result");
 
-    checkInput()
-})
+    const product = firstInput * secondInput;
+    result.textContent = product;
+    clickedOperation = multiplication.textContent;
 
-multiplication.addEventListener('click', () => {
-    let firstInput = parseFloat(document.querySelector('#first-input').value)
-    let secondInput = parseFloat(document.querySelector('#second-input').value)
-    let result = document.querySelector('.math-result')
+    checkInput();
+});
 
-    const sum = firstInput * secondInput
-    result.textContent = sum
+division.addEventListener("click", () => {
+    let firstInput = parseFloat(document.querySelector("#first-input").value);
+    let secondInput = parseFloat(document.querySelector("#second-input").value);
+    let result = document.querySelector(".math-result");
 
-    checkInput()
-})
+    const quotient = firstInput / secondInput;
+    result.textContent = quotient;
+    clickedOperation = division.textContent;
 
-division.addEventListener('click', () => {
-    let firstInput = parseFloat(document.querySelector('#first-input').value)
-    let secondInput = parseFloat(document.querySelector('#second-input').value)
-    let result = document.querySelector('.math-result')
+    checkInput();
+});
 
-    const sum = firstInput / secondInput
-    result.textContent = sum
+clearBtn.addEventListener("click", () => {
+    document.querySelector("#first-input").value = "";
+    document.querySelector("#second-input").value = "";
 
-    checkInput()
-})
-
-clearBtn.addEventListener('click', () => {
-    document.querySelector('#first-input').value = ''
-    document.querySelector('#second-input').value = ''
-
-    errMsg1.textContent = ''
-    errMsg2.textContent = ''
-    result.textContent = ''
-})
+    errMsg1.textContent = "";
+    errMsg2.textContent = "";
+    result.textContent = "";
+});
 
 // input event listeners on input elements
-firstInput.addEventListener('input', () => {
-    errMsg1.textContent = ''
-})
+firstInput.addEventListener("input", () => {
+    errMsg1.textContent = "";
+});
 
-secondInput.addEventListener('input', () => {
-    errMsg2.textContent = ''
-})
+secondInput.addEventListener("input", () => {
+    errMsg2.textContent = "";
+});
 
 // check input function
 function checkInput() {
-    let firstInput = document.querySelector('#first-input').value
-    let secondInput = document.querySelector('#second-input').value
+    let firstInput = document.querySelector("#first-input").value;
+    let secondInput = document.querySelector("#second-input").value;
 
-    if (firstInput === '' && secondInput === '') {
-        errMsg1.textContent = 'Provide a value'
-        errMsg2.textContent = 'Provide a value'
-        result.textContent = ''
-    } else if (firstInput === '') {
-        errMsg1.textContent = 'Provide a value'
-        result.textContent = ''
-    } else if (firstInput != '' && secondInput != '') {
-        errMsg1.textContent = ''
-        errMsg2.textContent = ''
-    } else if (secondInput === '') {
-        errMsg2.textContent = 'Provide a value'
-        result.textContent = ''
+    if (firstInput === "" && secondInput === "") {
+        errMsg1.textContent = "Provide a value";
+        errMsg2.textContent = "Provide a value";
+        result.textContent = "";
+    } else if (firstInput === "") {
+        errMsg1.textContent = "Provide a value";
+        result.textContent = "";
+    } else if (firstInput != "" && secondInput != "") {
+        errMsg1.textContent = "";
+        errMsg2.textContent = "";
+    } else if (secondInput === "") {
+        errMsg2.textContent = "Provide a value";
+        result.textContent = "";
     }
 }
+
+// save calculations
+saveBtn.addEventListener("click", () => {
+    let firstInputNum = parseFloat(document.querySelector("#first-input").value);
+    let secondInputNum = parseFloat(document.querySelector("#second-input").value);
+    let firstInput = document.querySelector("#first-input").value;
+    let secondInput = document.querySelector("#second-input").value;
+    const sum = `${firstInputNum} ${clickedOperation} ${secondInputNum}`;
+
+    if (clickedOperation === undefined && secondInput === "" && firstInput === "") {
+        savedCal.textContent += "";
+    } 
+    else if (firstInput === "" || secondInput === "") {
+        savedCal.textContent += "";
+    } else if (clickedOperation !== undefined  && secondInput === "" && firstInput === "") {
+        savedCal.textContent += "";
+    } 
+    else {
+        savedCal.textContent += `${sum} = ${result.textContent} || `;
+        clickedOperation = undefined;
+    }
+});
+
